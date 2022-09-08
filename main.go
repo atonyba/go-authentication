bee7d3e3f2000a249efa8b5b608bd2bb8520fad3
Tony Balogh <atonyba@hotmail.com>
2022-09-07 22:14:47 -0700

    Added server port as env variable

--- main.go
+++ main.go
@@ -11,26 +11,21 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const (
-	SERVER_PORT = "8080"
-)
-
-
 var authProvider providers.AzureB2CProvider
 
 func main() {
 	err := godotenv.Load(".env")
-	if err != nil{
+	if err != nil {
 		log.Println("No .env file found. Using system environment variables.")
 	}
 
 	config := &config.GlobalConfig{
 		AuthorizationState: os.Getenv("AUTH_STATE"),
-		TenantName:os.Getenv("TENANT_NAME"),
-		TenantId:os.Getenv("TENANT_ID"),
-		PolicyName:os.Getenv("POLICY_NAME"),
-		B2cClientId:os.Getenv("B2C_CLIENTID"),
-		B2cClientSecret:os.Getenv("B2C_CLIENTSECRET"),
+		TenantName:         os.Getenv("TENANT_NAME"),
+		TenantId:           os.Getenv("TENANT_ID"),
+		PolicyName:         os.Getenv("POLICY_NAME"),
+		B2cClientId:        os.Getenv("B2C_CLIENTID"),
+		B2cClientSecret:    os.Getenv("B2C_CLIENTSECRET"),
 	}
 
 	authProvider.Initialize(config)
@@ -40,8 +35,8 @@ func main() {
 	http.HandleFunc("/oauth/receive", authProvider.Complete)
 	http.HandleFunc("/oauth/verify", authProvider.Verify)
 
-	log.Println("Starting web service on port", SERVER_PORT)
-	http.ListenAndServe(fmt.Sprintf(":%s", SERVER_PORT), nil)
+	log.Println("Starting web service on port", os.Getenv("SERVER_PORT"))
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT")), nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
